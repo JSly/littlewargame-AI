d5e34f1457171ff261d515e454eec8625cea9ab1
JSly <iwsmilam@gmail.com>
2014-07-14 17:40:15 -0500

    No longer finds enemies through finding mines/towers. Uses built in start location of enemy. Builds a Tower at base in the beginning for defense Smarter random building positions Updated JS to use JQuery in some places Removed attacking 1 at a time after finding enemy Various other changes

--- Tower Defense AI.js
+++ Tower Defense AI.js
@@ -2,21 +2,34 @@
 var randomNumber = function(highestNum) {
 	return Math.floor((Math.random() * highestNum) + 1)
 }
-
-//Uses a random number 1-4 to return a move object that is around the x/y location passed
-var randomBuildingPosition = function(varX,varY) {
-	var fourPositions = randomNumber(4);
-	if (fourPositions == 1) {	
-		return {x: varX - 4, y: varY}; //Left
-	} else if (fourPositions == 2) {
-		return {x: varX + 3, y: varY}; //Right
-	} else if (fourPositions == 3) {
-		return {x: varX, y: varY - 4}; //Top
-	} else if (fourPositions == 4) {
-		return {x: varX, y: varY + 3}; //Bottom
-	}
+//Returns a Random Number that can be positive or negative
+var randomPosNegNumber = function(highestNum) {
+	return (randomNumber(2) == 1) ? randomNumber(highestNum)*-1 : randomNumber(highestNum);
 }
 
+//Returns a move object that is around the x/y location passed
+var randomBuildingPosition = function(varX,varY) {
+	var changePosition = function(position){
+		var randomThree = randomNumber(3);
+		var newPosition = position;
+		//Changes the position passed to stay the 
+		if (randomThree == 1) {
+			newPosition = position+randomPosNegNumber(3); //Same then +/- 1,2, or 3
+		} else if (randomThree == 2) {
+			newPosition = position-4+randomPosNegNumber(3); //-4 then +/- 1,2, or 3
+		} else if (randomThree == 3) {
+			newPosition = position+4+randomPosNegNumber(3); //+4 then +/- 1,2, or 3
+		}
+		return newPosition
+	}
+	var buildPositionObj = {
+		x: changePosition(varX),
+		y: changePosition(varY)
+	};
+	return buildPositionObj;
+}
+
+var players = scope.getArrayOfPlayerNumbers();
 // get my own player number; we need this to check if a unit is mine or not
 var myPlayerNumber = scope.getMyPlayerNumber();
 // get my team number; we can compare this to a units team number to check if its allied or not
@@ -30,42 +43,58 @@ var workers = scope.getUnits({type: "Worker", player: myPlayerNumber});
 var miningWorkers = scope.getUnits({type: "Worker", player: myPlayerNumber, order: "Mine"});
 var fightingUnits = scope.getUnits({notOfType: "Worker", player: myPlayerNumber});
 var castles = scope.getBuildings({type: "Castle", player: myPlayerNumber});
+var teamCastles = scope.getBuildings({type: "Castle", team: myTeamNumber});
 var barracks = scope.getBuildings({type: "Barracks", player: myPlayerNumber});
 var houses = scope.getBuildings({type: "House", player: myPlayerNumber});
+var towers = scope.getBuildings({type: "Watchtower", player: myPlayerNumber});
 //Including teammates towers
-var towers = scope.getBuildings({type: "Watchtower", team: myTeamNumber});
+var teamTowers = scope.getBuildings({type: "Watchtower", team: myTeamNumber});
 var finishedHouses = scope.getBuildings({type: "House", player: myPlayerNumber, onlyFinshed: true});
 var finishedBarracks = scope.getBuildings({type: "Barracks", player: myPlayerNumber, onlyFinshed: true});
 var mines = scope.getBuildings({type: "Goldmine"});
 var enemyUnits = scope.getUnits({enemyOf: myPlayerNumber});
 
+//Starts with all players
+var enemies = scope.getArrayOfPlayerNumbers();
+//Removes all players from 'enemies' that are on your team
+$.each(players, function(index, playerNumber) {
+	var startPosition = scope.getStartLocationForPlayerNumber(playerNumber)
+	$.each(teamCastles, function(index, castle) {
+		var castlePosition = {x:castle.getX(), y:castle.getY()}
+		if (startPosition.x == castle.getX()-1 && startPosition.y == castle.getY()-1) {
+			enemies = jQuery.grep(enemies, function(value) {
+				return value != playerNumber;
+			});
+		}
+	});
+});
+
 var nearestMineFromCastle = null;
 var nearestMineDistanceFromCastle = 99999;
 
-// look for the next gold mine from our castle (slightly modified from default still will not look for another mine after the first is depleted)
-if (castles.length >= 1) {
-	for (var i = 0; i < mines.length; i++) {
-		var mine = mines[i];
-		var dist = Math.pow(mine.getX() - castles[0].getX(), 2) + Math.pow(mine.getY() - castles[0].getY(), 2);
-		if (dist < nearestMineDistanceFromCastle) {
-			nearestMineFromCastle = mine;
-			nearestMineDistanceFromCastle = dist;
-		}
+$.each(mines, function(index, mine) {
+	var dist = Math.pow(mine.getX() - castles[0].getX(), 2) + Math.pow(mine.getY() - castles[0].getY(), 2);
+	if (dist < nearestMineDistanceFromCastle) {
+		nearestMineFromCastle = mine;
+		nearestMineDistanceFromCastle = dist;
 	}
-}
+});
 
 // order all idle workers to mine from the nearest gold mine
 if (nearestMineFromCastle && idleWorkers.length > 0) {
 	scope.order("Mine", idleWorkers, {unit: nearestMineFromCastle});
 }
 
-// if the castle is idle, order to make a worker
-if (castles.length >= 1 && !castles[0].getUnitTypeNameInProductionQueAt(1) && workers.length < 10) {
+// if the castle is idle, order to make workers up to 7
+if (castles.length >= 1 && !castles[0].getUnitTypeNameInProductionQueAt(1) && workers.length < 7) {
+	scope.order("Train Worker", [castles[0]]);
+//After primary base is established (tower/barracks) create workers up to 10
+} else if (castles.length >= 1 && !castles[0].getUnitTypeNameInProductionQueAt(1) && finishedBarracks.length > 0 && workers.length < 10) {
 	scope.order("Train Worker", [castles[0]]);
 }
  
 // if any enemy units in sight and we have fighting units, order them to attack them
-if (enemyUnits.length > 0 && fightingUnits.length > 0) {
+if (enemyUnits.length > 0 && fightingUnits.length > 3) {
 	scope.order("AMove", fightingUnits, scope.getCenterOfUnits(enemyUnits));
 }
 
@@ -73,12 +102,19 @@ if (enemyUnits.length > 0 && fightingUnits.length > 0) {
 if (castles.length >= 1 && houses.length == 0) {
 	//Added randomBuildingPosition around castle
 	scope.order("Build House", workers, randomBuildingPosition(castles[0].getX(),castles[0].getY()));
+} 
+//If the building of house 1 fails this is a fall through
+else if (houses.length == 1 && finishedHouses.length == 0 && workers.length == 7) {
+	scope.order("Moveto", workers, houses[0].getX(),houses[0].getY());
 }
- 
-// if we have at least one finished house (= we can make barracks), make a barracks (at some fixed position left of out castle, which will lead to some problems; you shouldnt do it like that)
-if (castles.length >= 1 && finishedHouses.length > 0 && barracks.length == 0) {
-	//Added randomBuildingPosition around castle
-	scope.order("Build Barracks", workers, randomBuildingPosition(castles[0].getX(),castles[0].getY()));
+
+// if we have at least one finished house make tower 
+if (castles.length >= 1 && finishedHouses.length > 0 && towers.length == 0) {
+	scope.order("Build Watchtower", workers, randomBuildingPosition(castles[0].getX(),castles[0].getY()));
+}
+// if we have at least one tower started build barracks
+if (castles.length >= 1 && towers.length > 0 && barracks.length == 0) {
+	scope.order("Build Barracks", miningWorkers, randomBuildingPosition(castles[0].getX(),castles[0].getY()));
 }
  
 //Trains 5 soldiers
@@ -86,43 +122,8 @@ if (finishedBarracks.length > 0  && fightingUnits.length < 5) {
 	scope.order("Train Soldier", [barracks[0]]);
 }
  
-//Checks to see if passed x/y is near a tower returns true/false based on if the x/y is within a distance of 20
-var nearTower = function(x,y) {	
-	var nearestTower = null;
-	var nearestTowerDistance = 99999;
-	for (var i = 0; i < towers.length; i++) {
-		var tower = towers[i];
-		var towerDistance = Math.pow(tower.getX() - x, 2) + Math.pow(tower.getY() - y, 2);
-		if (towerDistance < nearestTowerDistance) {
-			nearestTower = tower;
-			nearestTowerDistance = towerDistance;
-		}
-	}
-	if (nearestTowerDistance < 20) {
-		return true
-	} else {
-		return false
-	}
-}
- 
-//After we have 5 units send them out to attack. They will search for the nearest mine and hang out there until they find an enemy of a worker starts building a tower near the mine
-//If a tower is being constructed or is constructed the Soldiers will move on to the next nearest mine until they will eventually find an enemy
-if (fightingUnits.length == 5) {	
-	var nearestMineFromFightingUnits = null;
-	var nearestMineDistanceFromFightingUnits = 99999;
-	var buildTowerNearMine = false;	
-	for (var i = 0; i < mines.length; i++) {	
-		var mine = mines[i];
-		var mineNearTower = nearTower(mine.getX(), mine.getY());		
-		var mineDistanceFromFightingUnits = Math.pow(mine.getX() - fightingUnits[0].getX(), 2) + Math.pow(mine.getY() - fightingUnits[0].getY(), 2);
-		if (mineDistanceFromFightingUnits < nearestMineDistanceFromFightingUnits && mine != nearestMineFromCastle && !mineNearTower && mineDistanceFromFightingUnits > 22) {
-			nearestMineFromFightingUnits = mine;
-			nearestMineDistanceFromFightingUnits = mineDistanceFromFightingUnits;
-			buildTowerNearMine = true;
-		}		
-	}	
-	if (buildTowerNearMine) {
-		scope.order("Build Watchtower", miningWorkers, randomBuildingPosition(nearestMineFromFightingUnits.getX(),nearestMineFromFightingUnits.getY()));
-	}		
-	scope.order("AMove", fightingUnits, {x: nearestMineFromFightingUnits.getX(), y: nearestMineFromFightingUnits.getY()});
+//Better Attack using new 'enemies' (only supports one enemy)
+if (fightingUnits.length == 5) {
+	var enemyPosition = scope.getStartLocationForPlayerNumber(enemies[0])	
+	scope.order("AMove", fightingUnits, enemyPosition);
 }
