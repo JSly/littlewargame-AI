a10b7d63ebc36d6f87ce86e0e5b5632f1ed23ec1
JSly <iwsmilam@gmail.com>
2014-07-15 22:11:25 -0500

    Moves to new mine after depletion Creates new castle by mine with crappy castleBuilder function

--- Attacking AI.js
+++ Attacking AI.js
@@ -26,67 +26,111 @@ var randomBuildingPosition = function(varX,varY) {
 		x: changePosition(varX),
 		y: changePosition(varY)
 	};
-	return buildPositionObj;
+	if (scope.positionIsPathable(buildPositionObj.x, buildPositionObj.y)) {
+		return buildPositionObj;
+	} else {
+		return randomBuildingPosition(varX,varY);
+	}
+}
+
+//Returns a move object surrounding a mine
+var buildCastle = function(varX,varY) {
+	var positionArr = [
+		{x: 0,y: 10},{x: 10,y: 0},{x: 0,y: -10},{x: -10,y: 0},
+		{x: 1,y: 10},{x: 10,y: 1},{x: 1,y: -10},{x: -10,y: 1},
+		{x: -1,y: 10},{x: 10,y: -1},{x: -1,y: -10},{x: -10,y: -1},
+		{x: 2,y: 10},{x: 10,y: 2},{x: 2,y: -10},{x: -10,y: 2},
+		{x: -2,y: 10},{x: 10,y: -2},{x: -2,y: -10},{x: -10,y: -2},
+		{x: 3,y: 9},{x: 9,y: 3},{x: 3,y: -9},{x: -9,y: 3},
+		{x: -3,y: 9},{x: 9,y: -3},{x: -3,y: -9},{x: -9,y: -3},
+		{x: 4,y: 8},{x: 8,y: 4},{x: 4,y: -8},{x: -8,y: 4},
+		{x: -4,y: 8},{x: 8,y: -4},{x: -4,y: -8},{x: -8,y: -4},
+		{x: 5,y: 7},{x: 7,y: 5},{x: 5,y: -7},{x: -5,y: 4},
+		{x: -5,y: 7},{x: 7,y: -5},{x: -5,y: -7},{x: -5,y: -4},
+		{x: 6,y: 6},{x: 6,y: -6},{x: -6,y: 6},{x: -6,y: -6},
+	];
+	var ranNum = randomNumber(positionArr.length-1);
+	var buildPositionObj = {
+		x: varX+positionArr[ranNum].x,
+		y: varY+positionArr[ranNum].y
+	};
+	if (scope.positionIsPathable(buildPositionObj.x, buildPositionObj.y)) {
+		return buildPositionObj;
+	} else {
+		return buildCastle(varX,varY);
+	}
 }
 
 var players = scope.getArrayOfPlayerNumbers();
-// get my own player number; we need this to check if a unit is mine or not
 var myPlayerNumber = scope.getMyPlayerNumber();
-// get my team number; we can compare this to a units team number to check if its allied or not
 var myTeamNumber = scope.getMyTeamNumber();
-// get my gold value
 var gold = scope.getGold();
-// get several units
+
 var idleWorkers = scope.getUnits({type: "Worker", player: myPlayerNumber, order: "Stop"});
 var workers = scope.getUnits({type: "Worker", player: myPlayerNumber});
-//Returns workers who are mining to prevent a worker who is building a tower to be sent to the next tower prior to completing theirs
 var miningWorkers = scope.getUnits({type: "Worker", player: myPlayerNumber, order: "Mine"});
 var fightingUnits = scope.getUnits({notOfType: "Worker", player: myPlayerNumber});
-var castles = scope.getBuildings({type: "Castle", player: myPlayerNumber});
-var teamCastles = scope.getBuildings({type: "Castle", team: myTeamNumber});
-var barracks = scope.getBuildings({type: "Barracks", player: myPlayerNumber});
-var houses = scope.getBuildings({type: "House", player: myPlayerNumber});
-var towers = scope.getBuildings({type: "Watchtower", player: myPlayerNumber});
-//Including teammates towers
-var teamTowers = scope.getBuildings({type: "Watchtower", team: myTeamNumber});
-var finishedHouses = scope.getBuildings({type: "House", player: myPlayerNumber, onlyFinshed: true});
-var finishedBarracks = scope.getBuildings({type: "Barracks", player: myPlayerNumber, onlyFinshed: true});
-var mines = scope.getBuildings({type: "Goldmine"});
 var enemyUnits = scope.getUnits({enemyOf: myPlayerNumber});
 
-//Starts with all players
-var enemies = scope.getArrayOfPlayerNumbers();
-//Removes all players from 'enemies' that are on your team
-$.each(players, function(index, playerNumber) {
-	var startPosition = scope.getStartLocationForPlayerNumber(playerNumber)
-	if (teamCastles.length > 0) {
-		$.each(teamCastles, function(index, castle) {
-			var castlePosition = {x:castle.getX(), y:castle.getY()}
-			if (startPosition.x == castle.getX()-1 && startPosition.y == castle.getY()-1) {
-				enemies = jQuery.grep(enemies, function(value) {
-					return value != playerNumber;
-				});
-			}
-		});
-	}
-});
+var castles = scope.getBuildings({type: "Castle", player: myPlayerNumber});
+var finishedCastles = scope.getBuildings({type: "Castle", player: myPlayerNumber, onlyFinshed: true});
+var teamCastles = scope.getBuildings({type: "Castle", team: myTeamNumber});
+var barracks = scope.getBuildings({type: "Barracks", player: myPlayerNumber});
+var finishedBarracks = scope.getBuildings({type: "Barracks", player: myPlayerNumber, onlyFinshed: true});
+var houses = scope.getBuildings({type: "House", player: myPlayerNumber});
+var finishedHouses = scope.getBuildings({type: "House", player: myPlayerNumber, onlyFinshed: true});
+var towers = scope.getBuildings({type: "Watchtower", player: myPlayerNumber});
+var mines = scope.getBuildings({type: "Goldmine"});
 
+if (!this.enemies) {
+	var enemies = scope.getArrayOfPlayerNumbers();
+	$.each(players, function(index, playerNumber) {
+		var startPosition = scope.getStartLocationForPlayerNumber(playerNumber)
+		if (teamCastles.length > 0) {
+			$.each(teamCastles, function(index, castle) {
+				var castlePosition = {x:castle.getX(), y:castle.getY()}
+				if (startPosition.x == castle.getX()-1 && startPosition.y == castle.getY()-1) {
+					enemies = jQuery.grep(enemies, function(value) {
+						return value != playerNumber;
+					});
+				}
+			});
+		}
+	});
+	this.enemies = enemies;
+}
+	
 var nearestMineFromCastle = null;
 var nearestMineDistanceFromCastle = 99999;
-
 if (castles.length >= 1) {
 	$.each(mines, function(index, mine) {
 		var dist = Math.pow(mine.getX() - castles[0].getX(), 2) + Math.pow(mine.getY() - castles[0].getY(), 2);
-		if (dist < nearestMineDistanceFromCastle) {
+		if (dist < nearestMineDistanceFromCastle && mine.unit.gold != 0) {
 			nearestMineFromCastle = mine;
 			nearestMineDistanceFromCastle = dist;
+		}
+	});
+}
+var secondNearestMineFromCastle = null;
+var secondNearestMineDistanceFromCastle = 99999;
+if (castles.length >= 1) {
+	$.each(mines, function(index, mine) {
+		var dist = Math.pow(mine.getX() - castles[0].getX(), 2) + Math.pow(mine.getY() - castles[0].getY(), 2);
+		if (dist < secondNearestMineDistanceFromCastle && dist != nearestMineDistanceFromCastle && mine.unit.gold != 0) {
+			secondNearestMineFromCastle = mine;
+			secondNearestMineDistanceFromCastle = dist;
 		}
 	});
 }
 
 // order all idle workers to mine from the nearest gold mine
 if (nearestMineFromCastle && idleWorkers.length > 0) {
-	scope.order("Mine", idleWorkers, {unit: nearestMineFromCastle});
+	$.each(workers, function(index, worker) {
+		if (worker.getCurrentOrderName() == "Stop") {
+			var workersMine = (index<10) ? nearestMineFromCastle : secondNearestMineFromCastle;
+			scope.order("Mine", [worker], {unit: workersMine});
+		}
+	});
 }
 
 // if the castle is idle, order to make workers up to 7
@@ -109,9 +153,9 @@ if (castles.length >= 1 && houses.length == 0) {
 	//Added randomBuildingPosition around castle
 	scope.order("Build House", workers, randomBuildingPosition(castles[0].getX(),castles[0].getY()));
 } 
-//If the building of house 1 fails this is a fall through
-else if (houses.length == 1 && finishedHouses.length == 0 && workers.length >= 7) {
-	scope.order("Moveto", workers, {x: houses[0].getX(), y:houses[0].getY()});
+//Build second house at new base
+else if (castles.length >= 2 && houses.length == 1) {
+	scope.order("Build House", miningWorkers, randomBuildingPosition(castles[1].getX(),castles[1].getY()));
 }
 
 // if we have at least one finished house make tower 
@@ -122,6 +166,21 @@ if (castles.length >= 1 && finishedHouses.length > 0 && towers.length == 0) {
 if (castles.length >= 1 && towers.length > 0 && barracks.length == 0) {
 	scope.order("Build Barracks", miningWorkers, randomBuildingPosition(castles[0].getX(),castles[0].getY()));
 }
+
+// Secondary base
+if (castles.length == 1 && finishedBarracks.length > 0 && gold > 400) {
+	// order all idle workers to mine from the nearest gold mine
+	scope.order("Build Castle", miningWorkers, buildCastle(secondNearestMineFromCastle.getX(),secondNearestMineFromCastle.getY()));
+}
+
+if (castles.length >= 2 && workers.length < 20) {
+	if (!castles[0].getUnitTypeNameInProductionQueAt(1)) {
+		scope.order("Train Worker", [castles[0]]);
+	}
+	if (!castles[1].getUnitTypeNameInProductionQueAt(1)) {
+		scope.order("Train Worker", [castles[1]]);
+	}
+}
  
 //Trains 5 soldiers
 if (finishedBarracks.length > 0  && fightingUnits.length < 5) {
@@ -130,6 +189,6 @@ if (finishedBarracks.length > 0  && fightingUnits.length < 5) {
  
 //Better Attack using new 'enemies' (only supports one enemy)
 if (fightingUnits.length == 5) {
-	var enemyPosition = scope.getStartLocationForPlayerNumber(enemies[0])	
+	var enemyPosition = scope.getStartLocationForPlayerNumber(this.enemies[0])	
 	scope.order("AMove", fightingUnits, enemyPosition);
 }
